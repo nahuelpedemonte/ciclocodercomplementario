1ba75e6e955c00235c796cfbf86ac53960908b86
n4huerules <rulesnahue@gmail.com>
2022-05-11 22:52:52 -0300

    desafio complementario coder

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt('Ingrese el numero que se repetira'));
+let i = 1; 
+
+while ( i <= numero ){
+    console.log('Este frase se repetio la cantidad indicada '+i);
+    i++;
+}
